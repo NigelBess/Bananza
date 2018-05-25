52e888f58310fdf0aa25eec27d5fdb77b368ae8a
NigelBess <nigelbess@gmail.com>
2018-05-24 20:14:11 -0700

    got dropdown menus working nicely

--- js/main.js
+++ js/main.js
@@ -1,7 +1,64 @@
-console.log("This is working");
-document.getElementById("menu").addEventListener("click",openMenu);
+var menus = document.getElementsByClassName("menu");
+var dropdowns = document.getElementsByClassName("dropdown");
+var menuOpen = false;
+var currentMenu;
+var mouseOverDropDown = false;
+var mouseOverMenu = false;
+for (i=0; i<menus.length;i++)
+{
+	menus[i].addEventListener("click",openMenu);
+	menus[i].onmouseover = function(){mouseOverMenu = true;}
+	menus[i].onmouseout = function(){mouseOverMenu = false;}
+
+
+}
+for (i=0;i<dropdowns.length;i++)
+{
+	dropdowns[i].onmouseover = function(){ mouseOverDropDown = true;}
+	dropdowns[i].onmouseout = function(){mouseOverDropDown = false;}
+}
+
+
 
 function openMenu()
-{
-	document.getElementById("dropdown").classList.toggle("active");
+{	
+	if(!menuOpen || (currentMenu!=this))
+	{
+		for (i=0;i<dropdowns.length;i++)
+			{
+				if(dropdowns[i].parentNode == this)
+				{	
+					dropdowns[i].classList.add("active");
+					menuOpen = true;
+				}
+				if (dropdowns[i].parentNode != this)
+				{
+					dropdowns[i].classList.remove("active");
+				}
+			}
+	} else if (!mouseOverDropDown && (currentMenu==this))
+	{	
+		closeAll();
+	}
+		currentMenu = this;
+}
+
+window.onclick = function(event)
+{	
+	if (menuOpen && !mouseOverMenu)
+		{	
+			closeAll();
+		}
+}
+function closeAll()
+{	
+	for(i=0;i<dropdowns.length;i++)
+	{
+		if (dropdowns[i].classList.contains("active"))
+		{
+			dropdowns[i].classList.remove("active");
+		}
+
+	}
+	menuOpen = false;
 }
